89ab15e2f7743d91951511497a4be0d0bab2d30c
lordfid <alfidasalsabilla@gmail.com>
2026-06-16 07:57:25 +0700

    Delete src/components directory

--- src/components/LoadingState.tsx
+++
@@ -1,7 +0,0 @@
-type LoadingStateProps = {
-  text?: string;
-};
-
-export function LoadingState({ text = "Sebentar, sedang membaca jejak jawabanmu..." }: LoadingStateProps) {
-  return <div className="loading-state">{text}</div>;
-}
